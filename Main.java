997d966c65ac970b3f6fc99d9ea902bc1f4ded09
LFYStch <lfystch01@gmail.com>
2025-10-28 17:32:06 -0400

    Refactor paintComponent to use drawMesh method
    Updated paintComponent method to call drawMesh instead of runTheCode.

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.awt.image.*;
 import java.util.*;
 import java.io.*;
 import javax.imageio.*;
-import MainCode.*;
+
 
 
 public class Main {
@@ -58,7 +58,7 @@ class dP extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g2d = (Graphics2D) g;
-        runTheCode(g2d);
+        drawMesh(sp.LFYS(0,0,25,0,i,i),g2d,texture1);
     }
     //No edits past here! >:(
    public void drawMesh(mesh ts, Graphics2D g2d, BufferedImage texture) {
